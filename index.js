204d07741cbf5b104f05c76762c56c2e91e0fda0
Artem Vovsya <avovsya@gmail.com>
2014-03-23 17:03:36 +0200

    XOAUTH2 IMAP authorization

--- index.js
+++ index.js
@@ -1,63 +1,34 @@
-var GMailInterface = require('./lib/gmail2'),
-    fs             = require('fs'),
-    _               = require('underscore'),
-    mailConfig = require('./config.json'),
-    client         = new GMailInterface(),
-    path           = require('path'),
+var path           = require('path'),
     express        = require('express'),
-    http           = require('http'),
-    app            = express();
+    port           = process.env.PORT || 3000,
+    app            = express(),
+    passport    = require('passport'),
+    mongoose = require('mongoose'),
+    config = require('./config');
 
-app.set('port', process.env.PORT || 3000);
+mongoose.connect(config.db.url);
+require('./lib/config/passport')(passport);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.cookieParser('scooby do'));
+app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.session({ secret: 'scooby do' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+require('./lib/routes')(app, passport);
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/list', function (req, res) {
-    var client = new GMailInterface();
-    client.connect(mailConfig, function () {
-        client.getList({
-            success: function (messages) {
-                //res.send(JSON.stringify(messages));
-                res.render('list', {
-                    messages: _.filter(messages, function (msg) {
-                        return msg.from && msg.from.length > 0
-                    })
-                });
-                //client.logout();
-            },
-            error: function (err) {
-                res.send(JSON.stringify(err));
-            }
-        });
-    });
-});
-
-app.get('/', function (req, res) {
-    client.connect(mailConfig, function () {
-    });
-
-    client.on('messages', function (msg) {
-        res.send(msg[0].body);
-    });
-
-    client.on('error', function (err) {
-        console.log('ERROR: ');
-    });
-});
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
+app.listen(port);
+console.log('Listening port: ', port);
